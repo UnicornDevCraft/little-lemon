2317879e564a873f6eef5ed26b2ca70ceea0fab5
UnicornDevCraft <kseniia.usyk@outlook.com>
2024-12-02 23:53:19 +0100

    Added blog page content

---
+++ js/blog.js
@@ -0,0 +1,11 @@
+// Changing active navtab onclick
+document.addEventListener("DOMContentLoaded", () => {
+    const linkTabs = document.querySelectorAll('.tab');
+    linkTabs.forEach(tab => {
+        tab.addEventListener('click', () =>{
+            linkTabs.forEach(tab => tab.classList.remove('active'));
+            tab.classList.add('active');
+
+        });
+    });
+});
